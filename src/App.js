cd1ae992e6df7c95d446d55eac5e9990ce6d6f45
yostenalewis1 <yostenalewis9@gmail.com>
2023-11-23 00:23:10 +0200

    form.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-export default function App()
-{
-  return <Logo />
+export default function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+    </div>
+  );
 }
 
-function Logo()
-{
- return <h1> 🌊 FAR AWAY 🍱</h1>
+function Logo() {
+  return <h1> 🌊 FAR AWAY 🍱</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip</h3>
+    </div>
+  );
 }
